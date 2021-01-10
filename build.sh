abac95a0867bd31896a1a423d06212b9e3137e10
fwiethof98 <wiethof.florian98@gmail.com>
2021-01-10 22:22:44 +0100

    Removed website icon due to error

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@ cd frontend-react
 npm install
 npm run build
 rm -r ../backend-django/static/js
+rm -r ../backend-django/static/assets
 cp -rf ./build/static/js ../backend-django/static/js
+cp -rf ./public/static/assets ../backend-django/static/assets
 
 cd ..
 python3 build.py
