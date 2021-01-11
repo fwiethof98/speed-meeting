46cc061f5761a557a732eaccef2cd789e1fe7682
Sven Opgenorth <sv.op@mailbox.org>
2021-01-11 20:37:47 +0800

    ics update pfad

--- frontend-react/src/feedback/SuccessContent.js
+++ frontend-react/src/feedback/SuccessContent.js
@@ -15,7 +15,7 @@ function SuccessContent(props) {
                 <br></br>
                 
                 <a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=MGI3cWMybmxwYTlhMWVrdnM4aTJncm9hMmQgN2ZubWxqN3Rqb2sxcWhkMmdiZ3ZyZWZ1b2NAZw&amp;tmsrc=7fnmlj7tjok1qhd2gbgvrefuoc%40group.calendar.google.com"><img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_de.gif"></img></a>
-                <a href="/assets/calendar/GATHR_Event.ics"> Download .ics</a>
+                <a href="/static/assets/calendar/GATHR_Event.ics"> Download .ics</a>
                 <p><br></br>Invitation to plattform will get to you via e-mail a few days before the event.</p>
                 <h3>What else can I do?</h3>
                 <p class="unterschrift">Tell your friends! - The more people join the event, the more fun it will be!</p>
