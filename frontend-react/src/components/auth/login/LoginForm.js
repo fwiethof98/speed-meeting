a119248183d826826f882883fe46ad8559eb4223
fwiethof98 <wiethof.florian98@gmail.com>
2021-01-18 18:42:58 +0100

    Prevented default event at register button on login page

--- frontend-react/src/components/auth/login/LoginForm.js
+++ frontend-react/src/components/auth/login/LoginForm.js
@@ -3,11 +3,15 @@ import {PersonForm} from '../../templates/Forms'
 
 function LoginForm(props) {
     const {entries, handleSubmit} = props
+    const handleRegisterButton = (event) => {
+        event.preventDefault()
+        window.location.href = '/register/'
+    }
     return <div>
         <form id="login-form">
             <PersonForm entries={entries} />
             <button className="btn btn-primary" onClick={handleSubmit}>Login</button>
-            <button className="btn btn-secondary" onClick={() => window.location.href = 'register/'}>Register</button>
+            <button className="btn btn-secondary" onClick={handleRegisterButton}>Register</button>
         </form>
     </div>
 }
