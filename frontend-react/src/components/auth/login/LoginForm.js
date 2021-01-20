9ef2afc37da9932da6b34d4f4bf43a2cfd767cf2
Sven Opgenorth <sv.op@mailbox.org>
2021-01-20 17:59:46 +0800

    css login

--- frontend-react/src/components/auth/login/LoginForm.js
+++ frontend-react/src/components/auth/login/LoginForm.js
@@ -12,14 +12,14 @@ function LoginForm(props) {
             <div className="col-sm-5">
                 <PersonForm entries={entries} />
             </div>
-            <div className="col-sm-5" style={{marginTop: 50}}>
-                <div class="flash_art">
+            <div className="col-sm-5 col-sm-5-login" style={{marginTop: 50}}>
+                <div class="flash_art_login">
                     <img src="/static/assets/img/flash_art.png" alt="GATHR" />
                 </div>
             </div>
-            <div className="col-sm-12">
-                <button className="btn btn-primary" onClick={handleSubmit}>Login</button>
-                <button className="btn btn-secondary" onClick={handleRegisterButton}>Register</button>
+            <div className="col-sm-12 col-sm-12-login">
+                <button className="btn btn-primary btn-login" onClick={handleSubmit}>Login</button>
+                <button className="btn btn-secondary btn-login" onClick={handleRegisterButton}>Register</button>
             </div>
         </form>
     </div>
